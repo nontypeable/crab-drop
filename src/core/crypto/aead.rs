e29481e081cd24564bf630179b3f489dcc9061bc
Andrey Sadkov <ad.sadkov@gmail.com>
2026-01-03 17:09:48 +0300

    feat(core): define AeadCipher trait and placeholder CryptoError

---
+++ src/core/crypto/aead.rs
@@ -0,0 +1,4 @@
+pub trait AeadCipher {
+    fn encrypt(&self, key: &[u8], nonce: &[u8],plaintext: &[u8], aad: &[u8]) -> Result<Vec<u8>, CryptoError>;
+    fn decrypt(&self, key: &[u8], nonce: &[u8],ciphertext: &[u8], aad: &[u8]) -> Result<Vec<u8>, CryptoError>;
+}
