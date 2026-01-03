9aa9d170ae045fbcddf7d59c630ec043f90de6eb
Andrey Sadkov <ad.sadkov@gmail.com>
2026-01-03 23:24:41 +0300

    fix(crypto): correct vector allocation

--- src/infrastructure/crypto/hkdf_sha256.rs
+++ src/infrastructure/crypto/hkdf_sha256.rs
@@ -8,7 +8,7 @@ impl KeyDerivation for HkdfSha256 {
     fn derive(&self, ikm: &[u8], info: &[u8], len: usize) -> Vec<u8> {
         let hk = Hkdf::<Sha256>::new(None, ikm);
 
-        let mut okm: Vec<u8> = Vec::with_capacity(len);
+        let mut okm: Vec<u8> = vec![0u8; len];
         hk.expand(&info, &mut okm)
             .expect("42 is a valid length for Sha256 to output");
 
